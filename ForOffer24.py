51a0f33d2bd39e7f37ddd44f743a99ae8f5ea3f3
oo363636 <836481252@qq.com>
2020-08-13 17:38:09 +0800

    ForOffer24:反转链表

--- ForOffer24.py
+++ ForOffer24.py
@@ -43,3 +43,29 @@ if __name__ == '__main__':
     node2.next = node3
     node3.next = None
     print(test.reverse_list(node1))
+
+
+"""def reverseList(self, head):
+		"""
+		:type head: ListNode
+		:rtype: ListNode
+		"""
+		# 递归终止条件是当前为空，或者下一个节点为空
+		if(head==None or head.next==None):
+			return head
+		# 这里的cur就是最后一个节点
+		cur = self.reverseList(head.next)
+		# 这里请配合动画演示理解
+		# 如果链表是 1->2->3->4->5，那么此时的cur就是5
+		# 而head是4，head的下一个是5，下下一个是空
+		# 所以head.next.next 就是5->4
+		head.next.next = head
+		# 防止链表循环，需要将head.next设置为空
+		head.next = None
+		# 每层递归函数都返回cur，也就是最后一个节点
+		return cur
+
+作者：wang_ni_ma
+链接：https://leetcode-cn.com/problems/fan-zhuan-lian-biao-lcof/solution/dong-hua-yan-shi-duo-chong-jie-fa-206-fan-zhuan-li/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。"""
