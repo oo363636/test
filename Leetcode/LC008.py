69270813d40e2ee418dd3cdf5fca3fa4b688cd8c
oo363636 <836481252@qq.com>
2020-11-12 09:50:47 +0800

    LC008:字符串转换整数

---
+++ Leetcode/LC008.py
@@ -0,0 +1,64 @@
+"""
+请你来实现一个 atoi 函数，使其能将字符串转换成整数。
+
+首先，该函数会根据需要丢弃无用的开头空格字符，直到寻找到第一个非空格的字符为止。接下来的转化规则如下：
+
+如果第一个非空字符为正或者负号时，则将该符号与之后面尽可能多的连续数字字符组合起来，形成一个有符号整数。
+假如第一个非空字符是数字，则直接将其与之后连续的数字字符组合起来，形成一个整数。
+该字符串在有效的整数部分之后也可能会存在多余的字符，那么这些字符可以被忽略，它们对函数不应该造成影响。
+注意：假如该字符串中的第一个非空格字符不是一个有效整数字符、字符串为空或字符串仅包含空白字符时，
+则你的函数不需要进行转换，即无法进行有效转换。
+
+在任何情况下，若函数不能进行有效的转换时，请返回 0
+
+本题中的空白字符只包括空格字符 ' ' 。
+假设我们的环境只能存储 32 位大小的有符号整数，那么其数值范围为 [−231,  231 − 1]。
+如果数值超过这个范围，请返回  INT_MAX (231 − 1) 或 INT_MIN (−231)
+"""
+
+
+INT_MAX = 2 ** 31 - 1
+INT_MIN = -2 ** 31
+
+class Solution:
+    def myAtoi(self, s: str) -> int:
+        automaton = Automaton()
+        for c in s:
+            automaton.get(c)
+        return automaton.sign * automaton.ans
+
+
+class Automaton:
+    # 自动机，或者DFA
+    def __init__(self):
+        self.state = 'start'
+        self.sign = 1
+        self.ans = 0
+        self.table = {
+            'start': ['start', 'signed', 'in_number', 'end'],
+            'signed': ['end', 'end', 'in_number', 'end'],
+            'in_number': ['end', 'end', 'in_number', 'end'],
+            'end': ['end', 'end', 'end', 'end']
+        }
+
+    def get_col(self, c):
+        if c.isspace():  # 检查字符串是否只包含空格，是则返回True，否则False
+            return 0
+        if c == '+' or c == '-':
+            return 1
+        if c.isdigit():  # 检查字符串是否只由数字组成，是则返回True，否则False
+            return 2
+        return 3
+
+    def get(self, c):
+        self.state = self.table[self.state][self.get_col(c)]
+        if self.state == 'in_number':
+            self.ans = self.ans * 10 + int(c)
+            self.ans = min(self.ans, INT_MAX) if self.sign == 1 else min(self.ans, -INT_MIN)
+        elif self.state == 'signed':
+            self.sign = 1 if c == '+' else -1
+
+
+test = Solution()
+a = test.myAtoi('-0021235')
+print(a)
